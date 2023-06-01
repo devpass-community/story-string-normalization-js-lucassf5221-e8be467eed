ca4da6a931a3dd20a4a5d678884d86a9c667c107
Lucas Schiavo <lucasschiavo26@gmail.com>
2023-06-01 14:29:33 -0300

    Atualizacao

--- removeAccent.js
+++ removeAccent.js
@@ -1,5 +1,10 @@
 function removeAccent(str) {
-    // Add your solution here!
+  // Add your solution here!
+  let helloWorldAccent = helloWorld
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+
+  console.log(helloWorldAccent)
 }
 
-module.exports = removeAccent;
+module.exports = removeAccent
